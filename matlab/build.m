8326002bf4afe6a34b24ec814c5909dff08e2595
Ralph Schleicher <rs@ralph-schleicher.de>
2016-05-08 18:05:27 +0000

    Add build rules for Octave.

--- matlab/build.m
+++ matlab/build.m
@@ -1,3 +1,3 @@
 clear cc4fat
 disp('mex -I.. cc4fat.c ../rs-rainflow.c rs-matrix-transpose.c');
-mex -I.. cc4fat.c ../rs-rainflow.c rs-matrix-transpose.c
+mex -v -I.. cc4fat.c ../rs-rainflow.c rs-matrix-transpose.c
